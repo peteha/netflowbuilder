e899f92a1283a297f5c360c42de90af9d90db6cd
peteha <peter@pggb.net>
2023-10-08 18:12:30 +1000

    Update

--- setflow.sh
+++ setflow.sh
@@ -15,7 +15,7 @@ do
     ssh $1 'pkill iperf3'
     sleep 1
     echo "Starting server on $1 on port $2"
-    ssh $1 'iperf3 -sD -p '$2'
+    ssh $1 'iperf3 -sD -p '$2''
     rtime=$(shuf -i 3-20 -n 1)
     echo "UDP Runtime is: $rtime"
     iperf3 -u -c $1 -p $2 -i 1 -t $rtime -b 10
