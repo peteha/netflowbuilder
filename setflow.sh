8a33877cbb530226fd749a066ec2d157f02c7c0c
peteha <peter@pggb.net>
2023-10-08 18:14:07 +1000

    Updated

--- setflow.sh
+++ setflow.sh
@@ -11,12 +11,12 @@ do
     ssh $1 'iperf3 -sD -p '$2''
     rtime=$(shuf -i 3-20 -n 1)
     echo "TCP Runtime is: $rtime"
-    iperf3 -c $1 -p $2 -i 1 -t $rtime
+    iperf3 -c $1 -p $2 -t $rtime
     ssh $1 'pkill iperf3'
     sleep 1
     echo "Starting server on $1 on port $2"
     ssh $1 'iperf3 -sD -p '$2''
     rtime=$(shuf -i 3-20 -n 1)
     echo "UDP Runtime is: $rtime"
-    iperf3 -u -c $1 -p $2 -i 1 -t $rtime -b 10
+    iperf3 -u -c $1 -p $2 -t $rtime -b 5M
 done
