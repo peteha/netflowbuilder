2d913110f179137f3fee5d3e7433651c7c567098
Peter Hauck <peter@pggb.net>
2023-10-08 17:52:19 +1000

    IPERF 3

--- setflow.sh
+++ setflow.sh
@@ -1,6 +1,18 @@
 echo "Server is: $1"
-
 echo "Killing any remote iperf servers on $1"
-ssh $1 <<'EOF'
-kill -9 $(ps -aux | grep iperf | awk '{print $2}')
-EOF
+
+ssh $1 'pkill iperf3'
+sleep 2
+echo "Starting server on $1 on port $2"
+ssh $1 'iperf3 -sD -p '$2''
+
+
+for (( ; ; ))
+do
+    rtime=$(shuf -i 3-20 -n 1)
+    echo "TCP Runtime is: $rtime"
+    iperf3 -c $1 -p $2 -i 1 -t $rtime
+    rtime=$(shuf -i 3-20 -n 1)
+    echo "UDP Runtime is: $rtime"
+    iperf3 -u -c $1 -p $2 -i 1 -t $rtime
+done
